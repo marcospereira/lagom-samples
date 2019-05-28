e0da979c23bffeb7bcdd6931902fd3438f804203
Renato Cavalcanti <renato@cavalcanti.be>
2019-05-28 15:22:23 +0200

    tweak jboss logging

--- shopping-cart/travis-scripts/build.sh
+++ shopping-cart/travis-scripts/build.sh
@@ -8,12 +8,14 @@ psql -c "CREATE USER shopping_cart WITH PASSWORD 'shopping_cart';" -U postgres
 SHOPPING_CART_SOURCES=$1
 BUILD_TOOL=$2
 
+# we need to pass org.jboss.logging.provider=slf4j, otherwise jboss logging, 
+# used by hibernate, will try to bind to log4j
+JBOSS_LOGGING_TWEAK="-Dorg.jboss.logging.provider=slf4j"
+
 if [ "$BUILD_TOOL" == "sbt" ]; then
-    bin/runSbtJob $SHOPPING_CART_SOURCES test 
+    bin/runSbtJob $SHOPPING_CART_SOURCES $JBOSS_LOGGING_TWEAK test 
 elif [ "$BUILD_TOOL" == "maven" ]; then
-    # we need to pass org.jboss.logging.provider=slf4j, otherwise jboss logging, 
-    # used by hibernate, will try to bind to log4j
-    mvn -f $SHOPPING_CART_SOURCES -Dorg.jboss.logging.provider=slf4j test 
+    mvn -f $SHOPPING_CART_SOURCES $JBOSS_LOGGING_TWEAK test 
 else
     echo "unknown build tool [$BUILD_TOOL]"
     exit 1
